8eb179064f0e38bc0181d82f3003c4dd9f91520f
Loh Siu Yin <siuyin@beyondbroadcast.com>
2018-09-12 12:27:58 +0000

    Added hakkanats

---
+++ cmd/hakkanats/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	//10 OMIT
+	"github.com/nats-io/go-nats"
+	"github.com/nats-io/go-nats/encoders/protobuf"
+	pb "github.com/siuyin/present-hakka_roundhouse/proto/arith"
+	//20 OMIT
+)
+
+//30 OMIT
+//go:generate protoc -I ../../proto/arith arith.proto --go_out=plugins=grpc:../../proto/arith
+func main() {
+	fmt.Println("hakka roundhouse pub/sub service")
+	nats.RegisterEncoder("protobuf", &protobuf.ProtobufEncoder{})
+	sumService() // 1 // HL
+	loadGen()    // 2 // HL
+	select {}    // wait forever
+}
+
+//40 OMIT
+//50 OMIT
+func loadGen() {
+	nc, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		log.Fatalf("loadGen conn: %v", err)
+	}
+	c, err := nats.NewEncodedConn(nc, "protobuf")
+	if err != nil {
+		log.Fatalf("loadGen encoded conn: %v", err)
+	}
+
+	go func() {
+		for {
+			req := pb.SumRequest{
+				A: int32(rand.Intn(99)),
+				B: int32(rand.Intn(99))}
+			c.Publish("my-topic", &req) // HL
+			fmt.Printf("Published: %v+%v\n", req.A, req.B)
+			time.Sleep(time.Second)
+		}
+	}()
+}
+
+//60 OMIT
+//70 OMIT
+func sumService() {
+	nc, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		log.Fatalf("sum conn: %v", err)
+	}
+	c, err := nats.NewEncodedConn(nc, "protobuf")
+	if err != nil {
+		log.Fatalf("sum encoded conn: %v", err)
+	}
+	c.Subscribe("my-topic", func(r *pb.SumRequest) { // HL
+		fmt.Printf("%v+%v = %v\n", r.A, r.B, r.A+r.B)
+	})
+}
+
+//80 OMIT
--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/siuyin/present-hakka_roundhouse
 
 require (
 	github.com/golang/protobuf v1.2.0
+	github.com/nats-io/go-nats v1.6.0
+	github.com/nats-io/nuid v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d
 	google.golang.org/grpc v1.15.0
 )
