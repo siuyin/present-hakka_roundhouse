cf2f33323aee13a79314c8be6a07cc1ac490d0bc
Loh Siu Yin <siuyin@beyondbroadcast.com>
2018-09-16 08:33:33 +0000

    Highlighted proto sample.

--- cmd/hakka/main.go
+++ cmd/hakka/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"fmt"
 
-	pb "github.com/siuyin/present-hakka_roundhouse/proto/arith"
+	pb "github.com/siuyin/present-hakka_roundhouse/proto/arith" // 1 // HL
 )
 
 //go:generate protoc -I ../../proto/arith arith.proto --go_out=plugins=grpc:../../proto/arith
 func main() {
 	fmt.Println("hakka roundhouse monolith")
-	res := sum(&pb.SumRequest{A: 2, B: 3})
+	res := sum(&pb.SumRequest{A: 2, B: 3}) // 2 // HL
 	fmt.Printf("2+3 = %v\n", res.Result)
 }
 
 func sum(r *pb.SumRequest) *pb.SumResponse {
-	return &pb.SumResponse{Result: r.A + r.B}
+	return &pb.SumResponse{Result: r.A + r.B} // 3 // HL
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,11 @@ module github.com/siuyin/present-hakka_roundhouse
 
 require (
 	github.com/golang/protobuf v1.2.0
+	github.com/nats-io/gnatsd v1.3.0 // indirect
 	github.com/nats-io/go-nats v1.6.0
 	github.com/nats-io/nuid v1.0.0 // indirect
-	github.com/siuyin/dflt v0.0.0-20170321044958-ed0bb9f404b8 // indirect
+	github.com/siuyin/dflt v0.0.0-20170321044958-ed0bb9f404b8
+	golang.org/x/crypto v0.0.0-20180910181607-0e37d006457b // indirect
 	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d
 	google.golang.org/grpc v1.15.0
 )
